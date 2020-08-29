f5dece4c01965bcffc60bd8da8438a789390dedd
Kyle Peeler <kylepeeler@gmail.com>
2020-08-29 01:30:41 -0400

    added dummy api route and custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import Layout from '../components/layout';
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <center>
+        <h1> Page Not Found... 😢</h1>
+      </center>
+    </Layout>
+  );
+}
