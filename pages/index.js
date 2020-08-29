3671edc88f3f7b0d678732bc2ea39c14bf5964b4
Kyle Peeler <kylepeeler@gmail.com>
2020-08-29 01:00:00 -0400

    listing markdown posts on homepage using graymatter

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
 import utilStyles from '../styles/utils.module.css';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -20,6 +31,20 @@ export default function Home() {
           <a href="https://github.com/kylepeeler"> Github </a>.
         </p>
         <p>👷‍♂️🚧 This site is a work in progress!</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={`${utilStyles.headingLg}`}>Blog</h2>
+        <ul className={`${utilStyles.list}`}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={`${utilStyles.listItem}`} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
