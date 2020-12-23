71f21310708563dc41c858f7bab4c71aad74e3dd
Kyle Peeler <kylepeeler@gmail.com>
2020-12-23 14:43:56 -0500

    remove unused import

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import NextLink from 'next/link';
 import styled from '@emotion/styled';
 import GradientText from './GradientText';
